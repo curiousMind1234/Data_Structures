28a82d009d9c4de407940cddcf3a1581371e578f
Gauri Raut <95969496+curiousMind1234@users.noreply.github.com>
2024-02-11 01:48:55 +0530

    Update code.cpp

--- Dynamic Programming/Rod Cutting Problem/code.cpp
+++ Dynamic Programming/Rod Cutting Problem/code.cpp
@@ -17,5 +17,60 @@ int cutRod(vector<int> &price, int n)
 {
 	vector<vector<int>>dp(n, vector<int>(n+1, -1));
 	return solve(n-1,n, price,dp);
-
 }
+
+Time Complexity: O(N*W)
+Reason: There are N*W states therefore at max ‘N*W’ new problems will be solved.
+Space Complexity: O(N*W) + O(N)
+Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*W)).
+****************************************************************************************
+
+//Tabulization
+int cutRod(vector<int> &price, int n)
+{
+	vector<vector<int>>dp(n, vector<int>(n+1, 0));
+	for(int N=0;N<=n;N++){
+		dp[0][N] = N * price[0];
+	}
+	for(int ind=1;ind<n;ind++){
+		for(int N=0;N<=n;N++){
+			int not_take = 0 + dp[ind-1][N];
+			int take = INT_MIN;
+			int rodLength = ind+1;
+			if(rodLength <= N){
+				take = price[ind] + dp[ind][N-rodLength];
+			}  
+			dp[ind][N] = max(take,not_take);
+		} 
+	}
+	return dp[n-1][n];
+}
+Time Complexity: O(N*W)
+Reason: There are two nested loops
+Space Complexity: O(N*W)
+*************************************************************************
+	
+//Space Optimization using single array
+int cutRod(vector<int> &price, int n)
+{
+	vector<vector<int>>dp(n, vector<int>(n+1, 0));
+	vector<int>prev(n+1,0);
+	for(int N=0;N<=n;N++){
+		prev[N] = N * price[0];
+	}
+	for(int ind=1;ind<n;ind++){
+		for(int N=0;N<=n;N++){
+          
+			int not_take = 0 + prev[N];
+			int take = INT_MIN;
+			int rodLength = ind+1;
+			if(rodLength <= N){
+				take = price[ind] + prev[N-rodLength];
+			}  
+			prev[N] = max(take,not_take);
+		} 
+	}
+	return prev[n];
+}
+TC : O(N*N)
+SC :O(N)
