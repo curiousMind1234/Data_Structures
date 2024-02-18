edb0e9e177fc2a8485f3f6ccfd4355446bef3175
Gauri Raut <95969496+curiousMind1234@users.noreply.github.com>
2024-02-18 21:19:59 +0530

    Update code.cpp

--- Valid Parentheses/code.cpp
+++ Valid Parentheses/code.cpp
@@ -1,44 +1,44 @@
 bool isValid(string s) {
-        stack<char> st;
-        char x;
-        for(int i=0;i< s.size();i++)
-        {
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
-            {
-                st.push(s[i]);
-                continue;
-            }
-            // IF current character is not opening
-            // bracket, then it must be closing. So stack
-            // cannot be empty at this point.
-            if(st.empty())return false;
-            switch(s[i])
-            {
-                case ')':
-                    x = st.top();
-                    st.pop();
-                    if(x =='[' || x =='{')
-                        return false;
-                    break;
-                
-                case ']':
-                    x = st.top();
-                    st.pop();
-                    if(x =='(' || x =='{')
-                        return false;
-                    break;
-                    
-                case '}':
-                    x = st.top();
-                    st.pop();
-                    if(x =='(' || x =='[')
-                        return false;
-                    break;     
-            }       
-        }
-        return st.empty();
+/**************1st Approach************************/
+stack<char>st;
+for(int i=0;i<s.length();i++){
+    if(st.empty()){
+        st.push(s[i]);
     }
-};
+    else {
+        if((st.top() =='(' && s[i] == ')')
+          || (st.top()=='[' && s[i] == ']')
+          || (st.top()=='{' && s[i] == '}')){
+              st.pop();
+          }
+          else{
+              st.push(s[i]);
+          }
+    }
+}
+if(st.empty())return true;
+return false;
+
+/********************2nd Approach************************/
+int i = -1;
+for(auto &ch : s){
+    if(ch == '(' ||ch == '{' || ch == '['){
+        s[++i] = ch;
+    }
+    else{
+        if(i>=0 && ((s[i] =='(' && ch == ')')
+          || (s[i] =='[' && ch == ']')
+          || (s[i]=='{' && ch == '}'))){
+              i--;
+          }
+          else {
+              return false;
+          }
+    }
+}
+return i==-1;
+}
+
 
 /*Algorithm: 
 
@@ -47,5 +47,13 @@ Now traverse the expression string exp.
 If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
 If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) then pop from stack and if the popped character is the matching starting bracket then fine else brackets are not balanced.
 After complete traversal, if there is some starting bracket left in stack then “not balanced”
+
+1st Approach :
+TC : O(n)
+SC : O(n) because of stack
+
+2nd Approach:
+TC : O(n)
+SC : O(1)
 
 */
