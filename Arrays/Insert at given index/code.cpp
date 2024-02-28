f8781cc21edec6b0c30a9916d9bc8f4a6bf87fad
Gauri Raut <95969496+curiousMind1234@users.noreply.github.com>
2024-02-28 23:27:57 +0530

    Create code.cpp

---
+++ Arrays/Insert at given index/code.cpp
@@ -0,0 +1,15 @@
+void insertAtIndex(int arr[], int sizeOfArray, int index, int element) {
+       if(index == sizeOfArray-1) {
+            arr[index] = element;
+            return;
+        }
+        arr[sizeOfArray-1] = -1;
+        
+        for(int i = sizeOfArray-1; i > index; i--) {  
+            int temp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = temp;
+        }
+        //Then we insert the given element at given index.
+        arr[index] = element;
+    }
