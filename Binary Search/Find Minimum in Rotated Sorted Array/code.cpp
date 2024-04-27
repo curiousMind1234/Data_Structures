9250e7b0181974314386dab608710e86f8e85c98
Gauri Raut <95969496+curiousMind1234@users.noreply.github.com>
2024-04-27 20:15:04 +0530

    Update code.cpp

--- Binary Search/Find Minimum in Rotated Sorted Array/code.cpp
+++ Binary Search/Find Minimum in Rotated Sorted Array/code.cpp
@@ -1,3 +1,33 @@
+//GFG Problem
+int minNumber(int arr[], int low, int high){
+    int first = low, last = high;
+    
+    if(arr[high] > arr[low] || low == high){
+        return arr[low];
+    }
+    
+    while(first<=last){
+        int mid = (first+last)/2;
+        if(arr[mid-1] > arr[mid]){
+            return arr[mid];
+        }
+        else if(arr[mid] > arr[mid+1]){
+            return arr[mid+1];
+        }
+        else{
+            if(arr[mid] > arr[last]){
+                first = mid + 1;
+            }
+            else{
+                last = mid - 1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+//LeetCode problem
 int findMin(vector<int>& nums) {
         
     // priority_queue<int,vector<int>,greater<int>>pq_min;
