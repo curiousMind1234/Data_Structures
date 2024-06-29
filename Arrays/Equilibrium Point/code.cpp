2fc85b4d5e319da7eccea4beb332c6502085d414
Gauri Raut <95969496+curiousMind1234@users.noreply.github.com>
2024-06-29 18:26:58 +0530

    Update code.cpp

--- Arrays/Equilibrium Point/code.cpp
+++ Arrays/Equilibrium Point/code.cpp
@@ -18,6 +18,25 @@ int equilibriumPoint(long long a[], int n) {
     return -1;
 }
 
+int findEquilibriumIndex(vector<int> &arr) {
+    int left=0, right=0;
+    
+    for(int i=0;i<arr.size();i++){
+        right = right + arr[i];
+    } 
+    
+    for(int i=0;i<arr.size();i++){
+        right -= arr[i];
+       
+        if(left == right){
+            return i;
+        } 
+        left += arr[i];
+    }
+    return -1;
+}
+
+
 
 Time Complexity: O(n)
 Auxiliary Space: O(1)
